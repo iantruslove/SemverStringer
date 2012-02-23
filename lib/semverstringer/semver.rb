aeda86f8bb8e9155a88d46eba3633ca86e615685
Ian Truslove <ian.truslove@gmail.com>
2012-02-23 15:36:28 -0700

    Refactored for readability: moved code from init into privates.

--- lib/semverstringer/semver.rb
+++ lib/semverstringer/semver.rb
@@ -1,24 +1,8 @@
 module Semverstringer
 	class Semver
 		def initialize(params={})
-			if (params.has_key? :major) || (params.has_key? :minor) || (params.has_key? :patch)
-				@major = get_valid_version_number(params, :major)
-				@minor = get_valid_version_number(params, :minor)
-				@patch = get_valid_version_number(params, :patch)
-			else
-				@major = @minor = 0
-				@patch = 1
-			end
-
-			if params.has_key? :build
-				build_params = (params[:build].is_a? Array) ? params[:build] : Array.new(1, params[:build])
-					
-				build_params.each do |param|
-					raise ArgumentError.new("Characters in build ID are not allowed") if /[^0-9A-Za-z-]/.match param.to_s
-				end
-
-				@build = build_params.join "."
-			end
+			@major, @minor, @patch = get_version_numbers_from params
+			@build = get_build_string_from params
 		end
 
 		def to_s
@@ -27,7 +11,7 @@ module Semverstringer
 			version
 		end
 
-		#TODO: Extract this out as a mixin
+		#TODO: Extract these out as a mixin?
 		private
 		def raise_if_invalid_version_number(hash, key)
 			if hash.has_key? key
@@ -42,6 +26,34 @@ module Semverstringer
 			(hash.has_key? key) ? hash[key] : 0
 		end
 
+		private
+		def get_version_numbers_from(params)
+			if (params.has_key? :major) || (params.has_key? :minor) || (params.has_key? :patch)
+				major = get_valid_version_number(params, :major)
+				minor = get_valid_version_number(params, :minor)
+				patch = get_valid_version_number(params, :patch)
+			else
+				major = minor = 0
+				patch = 1
+			end
+
+			[major, minor, patch]
+		end
+
+		private
+		def get_build_string_from(params) 
+			if params.has_key? :build
+				build_params = (params[:build].is_a? Array) ? params[:build] : Array.new(1, params[:build])
+					
+				build_params.each do |param|
+					raise ArgumentError.new("Characters in build ID are not allowed") if /[^0-9A-Za-z-]/.match param.to_s
+				end
+
+				return build_params.join "."
+			else
+				return nil
+			end
+		end
 	end
 end
 
