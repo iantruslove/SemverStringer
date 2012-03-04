ddb581dfdf06a2c80f09bae1f39069034ab8141d
Ian Truslove <ian.truslove@gmail.com>
2012-03-04 11:05:23 -0700

    Refactored a little bit

--- lib/semver_stringer/semver.rb
+++ lib/semver_stringer/semver.rb
@@ -1,7 +1,7 @@
 module SemverStringer
 
-	class Semver
-	  include Comparable
+  class Semver
+    include Comparable
 
     class Substring
       include Comparable
@@ -22,7 +22,7 @@ module SemverStringer
         first_head = first_components.shift
         second_head = second_components.shift
 
-        head_comparison = Semver::compareVersionAtoms first_head, second_head
+        head_comparison = compare_version_atoms first_head, second_head
         if head_comparison == 0
           if first_components.length > 0 or second_components.length > 0 
             return Substring::compareVersionStrings first_components.join('.'), second_components.join('.')
@@ -31,6 +31,22 @@ module SemverStringer
           end
         else 
           return head_comparison
+        end
+      end
+
+      def self.compare_version_atoms(first, second)
+        if first == nil and second == nil
+          return nil
+        elsif first != nil and second == nil
+          return 1
+        elsif first == nil and second != nil
+          return -1 
+        else
+          unless first.match /[^0-9]/ or second.match /[^0-9]/
+            return first.to_i <=> second.to_i
+          else
+            return first <=> second
+          end
         end
       end
 
@@ -44,11 +60,53 @@ module SemverStringer
     # @option params [Integer] :patch Patch version number
     # @option params [String, Integer, Array<String, Integer>] :pre Pre-release identifier(s)
     # @option params [String, Integer, Array<String, Integer>] :build Build number identifier(s)
-		def initialize(params={})
-			@major, @minor, @patch = get_version_numbers_from params
-			@build = get_build_string_from params
-			@pre = get_pre_string_from params
-		end
+    def initialize(params={})
+      @major, @minor, @patch = get_version_numbers_from params
+      @build = get_build_string_from params
+      @pre = get_pre_string_from params
+    end
+
+
+    # All the semver.org comparison rules are expressed by this operator
+    def <=>(other) 
+
+      versionComparison = Substring.new(get_version_string) <=> Substring.new(other.get_version_string)
+      (return versionComparison) if versionComparison != 0
+
+      other_pre = other.instance_variable_get('@pre')
+      if @pre != nil and other_pre == nil
+        return -1
+      elsif @pre == nil and other_pre != nil
+        return 1
+      elsif @pre != nil and other_pre != nil
+        pre_comparison = Substring.new(@pre) <=> Substring.new(other_pre)
+        (return pre_comparison) if pre_comparison != 0
+      end
+
+      other_build = other.instance_variable_get('@build')
+      if @build == nil and other_build != nil
+        return -1
+      elsif @build != nil and other_build == nil
+        return 1
+      elsif @build != nil and other_build != nil
+        build_comparison = Substring.new(@build) <=> Substring.new(other_build)
+        (return build_comparison) if build_comparison != 0
+      end
+
+      return 0
+    end
+
+    def get_version_string
+      "#{@major}.#{@minor}.#{@patch}"
+    end
+
+    def get_pre_string
+      @pre
+    end
+
+    def get_build_string
+      @build
+    end
 
     # Returns a string representation of the SemVer
     #
@@ -56,129 +114,65 @@ module SemverStringer
     #   SemverStringer::Semver.new(:major=>2, :pre=>"alpha").to_s  #=> "2.0.0-alpha"
     #
     # @return [String]
-		def to_s
-			version = "#{@major}.#{@minor}.#{@patch}"
-			version << "-#{@pre}" unless @pre == nil
-			version << "+#{@build}" unless @build == nil
-			version
-		end
-
-    def compareVersionAtoms(first, second)
-      return Semver::compareVersionAtoms first, second
+    def to_s
+      version = get_version_string
+      version << "-#{get_pre_string}" unless @pre == nil
+      version << "+#{get_build_string}" unless @build == nil
+      version
     end
 
-    def self.compareVersionAtoms(first, second)
-      if first == nil and second == nil
-        return nil
-      elsif first != nil and second == nil
-        return 1
-      elsif first == nil and second != nil
-        return -1 
+    private
+    def raise_if_invalid_version_number(hash, key)
+      if hash.has_key? key
+        raise ArgumentError.new("Positive integers only") if hash[key] < 0
+        raise ArgumentError.new("Integral numbers only") if ! hash[key].integer? 
+      end
+    end
+
+    def get_valid_version_number(hash, key) 
+      raise_if_invalid_version_number(hash, key)
+      (hash.has_key? key) ? hash[key] : 0
+    end
+
+    def get_version_numbers_from(params)
+      if (params.has_key? :major) || (params.has_key? :minor) || (params.has_key? :patch)
+        major = get_valid_version_number(params, :major)
+        minor = get_valid_version_number(params, :minor)
+        patch = get_valid_version_number(params, :patch)
       else
-        unless first.match /[^0-9]/ or second.match /[^0-9]/
-          return first.to_i <=> second.to_i
+        major = minor = 0
+        patch = 1
+      end
+
+      [major, minor, patch]
+    end
+
+    def get_pre_string_from(params) 
+      return get_optional_string_from params, :pre
+    end
+
+    def get_build_string_from(params) 
+      return get_optional_string_from params, :build
+    end
+
+    def get_optional_string_from(params, type)
+      disallowed_chars = /[^0-9A-Za-z-]/
+
+        if params.has_key? type
+          identifiers = scalar_to_array params[type]
+
+          identifiers.each do |param|
+            raise ArgumentError.new("Characters in #{type} ID are not allowed") if disallowed_chars.match param.to_s
+          end
+
+          return identifiers.join "."
         else
-          return first <=> second
+          return nil
         end
-      end
     end
 
-    # All the semver.org comparison rules are expressed by this operator
-		def <=>(other) 
-		  if @major != other.instance_variable_get('@major') 
-        return @major <=> other.instance_variable_get('@major')
-      elsif @minor != other.instance_variable_get('@minor')
-        return @minor <=> other.instance_variable_get('@minor') 
-      elsif @patch != other.instance_variable_get('@patch')
-        return @patch <=> other.instance_variable_get('@patch') 
-      else
-
-        other_pre = other.instance_variable_get('@pre')
-        if @pre or other_pre
-          if @pre == nil and other_pre != nil
-            return 1
-          elsif @pre != nil and other_pre == nil
-            return -1
-          else
-            return compareVersionAtoms(@pre, other_pre)
-          end
-        end
-
-
-        other_build = other.instance_variable_get('@build')
-        if @build or other_build
-          if @build == nil and other_build != nil
-            return -1
-          elsif @build != nil and other_build == nil
-            return 1
-          else
-            return compareVersionAtoms(@build, other_build)
-          end
-        end
-
-        return 0
-      end
+    def scalar_to_array(scalar)
+      (scalar.is_a? Array) ? scalar : Array.new(1, scalar)
     end
-
-		private
-		def raise_if_invalid_version_number(hash, key)
-			if hash.has_key? key
-				raise ArgumentError.new("Positive integers only") if hash[key] < 0
-				raise ArgumentError.new("Integral numbers only") if ! hash[key].integer? 
-			end
-		end
-
-		private
-		def get_valid_version_number(hash, key) 
-			raise_if_invalid_version_number(hash, key)
-			(hash.has_key? key) ? hash[key] : 0
-		end
-
-		private
-		def get_version_numbers_from(params)
-			if (params.has_key? :major) || (params.has_key? :minor) || (params.has_key? :patch)
-				major = get_valid_version_number(params, :major)
-				minor = get_valid_version_number(params, :minor)
-				patch = get_valid_version_number(params, :patch)
-			else
-				major = minor = 0
-				patch = 1
-			end
-
-			[major, minor, patch]
-		end
-
-		private
-		def get_pre_string_from(params) 
-			return get_optional_string_from params, :pre
-		end
-
-		private
-		def get_build_string_from(params) 
-			return get_optional_string_from params, :build
-		end
-
-		private
-		def get_optional_string_from(params, type)
-			disallowed_chars = /[^0-9A-Za-z-]/
-
-			if params.has_key? type
-				identifiers = scalar_to_array params[type]
-					
-				identifiers.each do |param|
-					raise ArgumentError.new("Characters in #{type} ID are not allowed") if disallowed_chars.match param.to_s
-				end
-
-				return identifiers.join "."
-			else
-				return nil
-			end
-		end
-	
-		private 
-		def scalar_to_array(scalar)
-			(scalar.is_a? Array) ? scalar : Array.new(1, scalar)
-		end
-	end
+  end
 end
-
